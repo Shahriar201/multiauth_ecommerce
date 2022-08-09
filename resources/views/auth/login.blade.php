024056f36068ee05d887989d85a4d27525c548af
shahriar201 <shahriarbrine201@gmail.com>
2022-08-09 23:03:50 +0600

    login page desing

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -28,7 +28,7 @@
     <meta name="description" content="Premium Quality and Responsive UI for Dashboard.">
     <meta name="author" content="ThemePixels">
 
-    <title>Starlight Responsive Bootstrap 4 Admin Template</title>
+    <title>Ecommerce Admin Panel</title>
 
     <!-- vendor css -->
     <link href="{{ asset('public/backend') }}/lib/font-awesome/css/font-awesome.css" rel="stylesheet">
@@ -44,18 +44,34 @@
     <div class="d-flex align-items-center justify-content-center bg-sl-primary ht-100v">
 
       <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white">
-        <div class="signin-logo tx-center tx-24 tx-bold tx-inverse">starlight <span class="tx-info tx-normal">admin</span></div>
-        <div class="tx-center mg-b-60">Professional Admin Template Design</div>
+        <div class="signin-logo tx-center tx-24 tx-bold tx-inverse">Ecommerce <span class="tx-info tx-normal">admin</span></div>
+        <div class="tx-center mg-b-60">Ecommerce Admin Login</div>
+        <form action="#" method="POST">
+            @csrf
+            <div class="form-group">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email Address">
+            </div>
 
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Enter your username">
-        </div><!-- form-group -->
-        <div class="form-group">
-          <input type="password" class="form-control" placeholder="Enter your password">
-          <a href="" class="tx-info tx-12 d-block mg-t-10">Forgot password?</a>
-        </div><!-- form-group -->
-        <button type="submit" class="btn btn-info btn-block">Sign In</button>
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
 
+            <div class="form-group">
+                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password') }}" required autocomplete="current-password" autofocus placeholder="Password">
+
+                @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+
+                <a href="#" class="tx-info tx-12 d-block mg-t-10">Forgot password?</a>
+            </div>
+            
+            <button type="submit" class="btn btn-info btn-block">Sign In</button>
+        </form>
         <div class="mg-t-60 tx-center">Not yet a member? <a href="page-signup.html" class="tx-info">Sign Up</a></div>
       </div><!-- login-wrapper -->
     </div><!-- d-flex -->
