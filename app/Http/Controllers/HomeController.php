e09e36304c1b7d165ee830baae16f3aa5bd7a79e
shahriar201 <shahriarbrine201@gmail.com>
2022-08-13 22:42:37 +0600

    frontend mastering done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('frontend.frontend_master');
+        return view('frontend.layouts.frontend_home');
     }
 }
