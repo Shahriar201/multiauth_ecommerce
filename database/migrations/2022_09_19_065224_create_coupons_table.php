2c8c18c865d9ad22ae9956e1634fa197c176b100
shahriar201 <shahriarbrine201@gmail.com>
2022-09-19 12:58:32 +0600

    coupon table and controller setup

---
+++ database/migrations/2022_09_19_065224_create_coupons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('coupon');
+            $table->string('discount')->nullable();
+            $table->tinyInteger('status')->nullable()->comment('1 = active, 0 = inactive');
+            $table->tinyInteger('created_by')->nullable();
+            $table->tinyInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
