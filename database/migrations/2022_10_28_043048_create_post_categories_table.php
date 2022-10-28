480293bf74f8ab733e92fa1789ddfd0ac50acd93
shahriar201 <shahriarbrine201@gmail.com>
2022-10-28 10:51:27 +0600

    post migration create

---
+++ database/migrations/2022_10_28_043048_create_post_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('post_category_name_en');
+            $table->string('post_category_name_bn');
+            $table->tinyInteger('created_by')->nullable();
+            $table->tinyInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_categories');
+    }
+}
---
+++ database/migrations/2022_10_28_043601_create_posts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_category_id');
+            $table->string('post_title_en');
+            $table->string('post_title_bn');
+            $table->text('post_details_en')->nullable();
+            $table->text('post_details_bn')->nullable();
+            $table->string('post_image')->nullable();
+            $table->tinyInteger('created_by')->nullable();
+            $table->tinyInteger('updated_by')->nullable();
+            $table->timestamps();
+
+            // foreign key
+            $table->foreign('post_category_id')->references('id')->on('post_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
